4dbff730e1f3b4474d19288e2bf103c711a5332c
HemantJangde <Jangdeh@gmail.com>
2025-11-18 08:36:22 +0530

    rady to deploy backend code in verceln

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,39 @@
 // config/db.js
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error("❌ MongoDB Connection Error:", error.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
-    process.exit(1);
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      bufferCommands: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }).then((mongoose) => mongoose);
   }
-};
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default connectDB;
+
